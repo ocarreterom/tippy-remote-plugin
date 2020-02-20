f726f7ba7e8e6ae49b0dfff6799a0b317742b032
Óscar Carretero <ocarreterom@gmail.com>
2020-02-20 19:55:35 +0100

    mock global fetch

--- index.test.js
+++ index.test.js
@@ -1,3 +1,12 @@
-test('test', () => {
+import tippy from 'tippy.js'
+import remote from '../dist'
+
+const remoteContent = '<div>Remote content</div>'
+
+jest.spyOn(global, 'fetch').mockImplementation(() => Promise.resolve({
+  text: () => remoteContent
+}))
+
+test('init', () => {
   expect(true).toBe(true)
 })
